2ab3239636fb5bf205f591e537b9cf92c04a8c7f
tpaul01@mica.edu <tpaul01@mica.edu>
2018-05-31 01:06:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-var name = "Susan"
+var name = "Joe"
 var height = "74"
 
-if (name==="Susan"){
-    console.log("the name is correct")
-}
-else{
-  console.log('expected the name to be susan')
-}
 // Don't worry about this
 module.exports = { name, height
 }
